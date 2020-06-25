3d3f1d0860a575bc42835ef6442a98c7ae6de371
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-26 00:18:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Main {
+
+    public static void main(String[] args) {
+        User newUser = new User();
+
+        newUser.UserName();
+        newUser.Email();
+        newUser.Town();
+    }
+}
---
+++ User.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class User {
+
+        Scanner user = new Scanner(System.in);
+    String UserName() {
+        System.out.print("Please, enter Your Name:" + "\t");
+        String name = user.nextLine();
+        return name;
+    }
+    String Email() {
+        System.out.print("Enter Your e-mail:"  + "\t\t\t");
+        String email = user.nextLine();
+        return email;
+    }
+    String Town () {
+        System.out.print("Enter Your town:" + "\t\t\t");
+        String town = user.nextLine();
+        return town;
+    }
+}
