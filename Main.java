ebd269de8e7c905dd91135ac7c5f153439c7f400
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-26 10:20:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         User newUser = new User();
+        Validator validatorEmail = new Validator()
 
         newUser.UserName();
         newUser.Email();
