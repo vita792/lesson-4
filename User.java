292857787c32ebf76391883d5f67327e41b71f81
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-26 10:48:43 +0300

    Update User.java

--- User.java
+++ User.java
@@ -2,20 +2,23 @@ package com.company;
 import java.util.Scanner;
 public class User {
 
+        String name;
+        Private String email;
+        Private String town;
         Scanner user = new Scanner(System.in);
     String UserName() {
         System.out.print("Please, enter Your Name:" + "\t");
-        String name = user.nextLine();
+        name = user.nextLine();
         return name;
     }
     String Email() {
         System.out.print("Enter Your e-mail:"  + "\t\t\t");
-        String email = user.nextLine();
+        email = user.nextLine();
         return email;
     }
     String Town () {
         System.out.print("Enter Your town:" + "\t\t\t");
-        String town = user.nextLine();
+        town = user.nextLine();
         return town;
     }
 }
