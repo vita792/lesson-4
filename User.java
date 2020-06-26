2da3b9c103c24aff292d66a8b47629d34dccb115
vita792 <59779169+vita792@users.noreply.github.com>
2020-06-26 08:19:53 +0300

    Update User.java

--- User.java
+++ User.java
@@ -10,12 +10,12 @@ public class User {
     }
     String Email() {
         System.out.print("Enter Your e-mail:"  + "\t\t\t");
-        String email = user.nextLine();
+        Private String email = user.nextLine();
         return email;
     }
     String Town () {
         System.out.print("Enter Your town:" + "\t\t\t");
-        String town = user.nextLine();
+        Private String town = user.nextLine();
         return town;
     }
 }
